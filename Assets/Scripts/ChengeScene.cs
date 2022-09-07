5b482605a6322991d682c8ff6b9629857ba8208a
MatubaraTooru <kurakura.ingen@gmail.com>
2022-09-07 19:23:40 +0900

    シーンが遷移するときにフェードアウトするように変更

--- Assets/Scripts/ChengeScene.cs
+++ Assets/Scripts/ChengeScene.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class ChengeScene : MonoBehaviour
 {
+    [SerializeField] Image _fadeoutImage;
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        _fadeoutImage.DOFade(1f, 1f).OnComplete(() => SceneManager.LoadScene(sceneName));
     }
 }
